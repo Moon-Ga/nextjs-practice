9f7917a672f5377f9a41414f7ce21551ea78aed6
MGanom <chewy111@naver.com>
2022-03-24 23:58:01 +0900

    3/24 practice

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
+
+const API_KEY = process.env.API_KEY;
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ["image.tmdb.org"],
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/movies",
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,49 @@
 import SEO from "../components/SEO";
+import Image from "next/image";
+import { useState, useEffect } from "react";
 
-export default function Home() {
+export default function Home({ popular }) {
+  // const [movies, setMovies] = useState();
+  // useEffect(() => {
+  //   const popular = async () => {
+  //     const response = await fetch(`/api/movies`);
+  //     return response.json();
+  //   };
+  //   popular().then((res) => setMovies(res.results));
+  // }, []);
+  // console.log(movies);
   return (
     <>
       <SEO title="Home" />
       <h1>THIS IS HOME</h1>
+      {popular ? (
+        popular.map((movie) => {
+          return (
+            <div key={movie.id}>
+              <Image
+                src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
+                alt={`${movie.original_title}`}
+                width={500}
+                height={500}
+              />
+              <div>{movie.original_title}</div>;
+            </div>
+          );
+        })
+      ) : (
+        <h4>Loading...</h4>
+      )}
     </>
   );
+}
+
+export async function getServerSideProps() {
+  const popular = await fetch(`http://localhost:3000/api/movies`)
+    .then((res) => res.json())
+    .then((res) => res.results);
+  return {
+    props: {
+      popular,
+    },
+  };
 }
