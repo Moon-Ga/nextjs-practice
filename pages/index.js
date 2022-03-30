f47b808ead342bc775c3d9988a251b1bf821acf9
MGanom <chewy111@naver.com>
2022-03-31 03:23:07 +0900

    add: infinite scroll

--- pages/index.js
+++ pages/index.js
@@ -7,8 +7,7 @@ import { fa1, fa2, fa3 } from "@fortawesome/free-solid-svg-icons";
 
 export default function Home({ data }) {
   const router = useRouter();
-  const [movies, setMovies] = useState([]);
-  const [page, setPage] = useState(2);
+  const [movies, setMovies] = useState(data.results);
   const ranking = [
     {
       icon: fa1,
@@ -33,33 +32,71 @@ export default function Home({ data }) {
     },
   ];
 
+  const [target, setTarget] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const page = 2;
   useEffect(() => {
-    if (movies.length === 0) {
-      setMovies(data.results);
-    } else {
-      const trend = async () => {
+    let observer;
+    if (target) {
+      const fetchData = async () => {
+        setIsLoading(true);
         const response = await fetch(`/api/movies/trend/${page}`);
-        return response.json();
+        const data = await response.json();
+        setMovies((prev) => prev.concat(data.results));
+        page++;
+        setIsLoading(false);
       };
-      trend().then((res) => setMovies((prev) => prev.concat(res.results)));
+      const onIntersect = async ([entry], observer) => {
+        if (entry.isIntersecting) {
+          observer.unobserve(entry.target);
+          await fetchData();
+          observer.observe(entry.target);
+        }
+      };
+      observer = new IntersectionObserver(onIntersect, { threshold: 1 });
+      observer.observe(target);
     }
-  }, [page]);
+    return () => observer && observer.disconnect();
+  }, [target]);
 
   return (
     <>
       <SEO title="Home" />
       <div className="container mx-auto my-14 px-14 grid gap-x-14 grid-cols-3">
+        {movies.map((movie, idx) => {
+          if (idx < 3) {
+            return (
+              <div key={movie.id + "a" + idx} className={ranking[idx].poster}>
+                <FontAwesomeIcon
+                  icon={ranking[idx].icon}
+                  className={ranking[idx].badge}
+                />
+                <Image
+                  src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                  placeholder="blur"
+                  blurDataURL
+                  alt={`${movie.original_title}`}
+                  layout="fill"
+                  onClick={() =>
+                    router.push(`/movies/${movie.original_title}/${movie.id}`)
+                  }
+                />
+              </div>
+            );
+          }
+        })}
+      </div>
+      <div className="container mx-auto px-14 grid gap-x-8 gap-y-16 grid-cols-3 md:grid-cols-4">
         {movies &&
           movies.map((movie, idx) => {
-            if (idx < 3) {
+            if (idx >= 3) {
               return (
-                <div key={movie.id} className={ranking[idx].poster}>
-                  <FontAwesomeIcon
-                    icon={ranking[idx].icon}
-                    className={ranking[idx].badge}
-                  />
+                <div
+                  key={movie.id + "a" + idx}
+                  className="relative aspect-[2/3] w-full cursor-pointer"
+                >
                   <Image
-                    src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
+                    src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
                     placeholder="blur"
                     blurDataURL
                     alt={`${movie.original_title}`}
@@ -73,29 +110,10 @@ export default function Home({ data }) {
             }
           })}
       </div>
-      <div className="container mx-auto px-14 grid gap-x-8 gap-y-16 grid-cols-3 md:grid-cols-4">
-        {movies &&
-          movies.map((movie, idx) => {
-            if (idx >= 3) {
-              return (
-                <div
-                  key={movie.id}
-                  className="relative aspect-[2/3] w-full cursor-pointer"
-                >
-                  <Image
-                    src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
-                    placeholder="blur"
-                    blurDataURL
-                    alt={`${movie.original_title}`}
-                    layout="fill"
-                    onClick={() =>
-                      router.push(`/movies/${movie.original_title}/${movie.id}`)
-                    }
-                  />
-                </div>
-              );
-            }
-          })}
+      <div ref={setTarget} className="flex justify-center mt-5 h-28">
+        {isLoading && (
+          <div className="animate-spin w-20 h-20 border-8 rounded-full border-t-blue-600"></div>
+        )}
       </div>
     </>
   );
